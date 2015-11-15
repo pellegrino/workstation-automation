5620c107ee9d2cfcdd82e4bba216393f2d2758d7
Vitor Pellegrino <vitorp@gmail.com>
2015-11-15 19:51:08 +0100

    Removing unecessary files

--- battleschool/playbooks/bash/bash-extras/ps1.bash
+++
@@ -1,3 +0,0 @@
-
-# Created using http://bashrcgenerator.com/
-export PS1="$(__git_ps1)\[\033[38;5;7m\]\A\[$(tput sgr0)\]\[\033[38;5;15m\]: \[$(tput sgr0)\]\[\033[38;5;11m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\[$(tput sgr0)\]\[\033[38;5;202m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\]:\[$(tput sgr0)\]\[\033[38;5;6m\][\w]:\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"
