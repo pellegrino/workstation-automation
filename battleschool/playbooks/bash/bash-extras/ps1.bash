72df9136a053ea010fad086f583e72e946ef180b
Vitor Pellegrino <vitorp@gmail.com>
2015-11-15 18:32:09 +0100

    Adds bash completion and git highlighting

---
+++ battleschool/playbooks/bash/bash-extras/ps1.bash
@@ -0,0 +1,3 @@
+
+# Created using http://bashrcgenerator.com/
+export PS1="$(__git_ps1)\[\033[38;5;7m\]\A\[$(tput sgr0)\]\[\033[38;5;15m\]: \[$(tput sgr0)\]\[\033[38;5;11m\]\u\[$(tput sgr0)\]\[\033[38;5;15m\]@\[$(tput sgr0)\]\[\033[38;5;202m\]\h\[$(tput sgr0)\]\[\033[38;5;15m\]:\[$(tput sgr0)\]\[\033[38;5;6m\][\w]:\[$(tput sgr0)\]\[\033[38;5;15m\] \[$(tput sgr0)\]"
