f21a93b7f536fd7698918636c1a9cfa565b802eb
Vitor Pellegrino <vitorp@gmail.com>
2015-12-15 09:18:47 +0100

    Update packages and installs emacs

---
+++ linux/playbooks/emacs.d/init.el
@@ -0,0 +1,11 @@
+(require 'package) (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/") t) (package-initialize)
+
+
+(defvar my-packages '(better-defaults
+                      projectile
+                      clojure-mode
+                      cider))
+
+(dolist (p my-packages)
+  (unless (package-installed-p p)
+    (package-install p)))
